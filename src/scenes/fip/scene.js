d2906612269ac563f378bab3cd0978f50369bb50
Destiny Ifehor <destiny.ifehor@interswitchgroup.com>
2024-09-30 13:19:40 +0100

    updated

--- src/scenes/fip/scene.js
+++ src/scenes/fip/scene.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
+import {ImageBackground, Text, View} from 'react-native';
 
-import { connect } from 'react-redux';
-import { APP_NAME, EXPIRED_SESSION_MESSAGE, USER } from '../../constants';
-import { ERROR_STATUS } from '../../constants/api';
-import { BLOCKER } from '../../constants/dialog-priorities';
-import UserSerializer from '../../serializers/resources/user';
+import {connect} from 'react-redux';
 import Platform from '../../services/api/resources/platform';
-import { retrieveAuthToken } from '../../utils/auth';
-import { flashMessage } from '../../utils/dialog';
-import handleErrorResponse from '../../utils/error-handlers/api';
-import { loadData, saveData } from '../../utils/storage';
+import {loadData} from '../../utils/storage';
 import AccountScene from '../agent/scenes/account';
 import LogoutScene from '../logout';
 import DisabledScene from '../misc/disabled-scene';
@@ -25,221 +18,222 @@ import SettingsScene from './scenes/settings';
 
 class OverlaySwitchClone extends React.Component {
   state = {
-    imageUri: null
-  }
+    imageUri: null,
+  };
 
   componentDidMount() {
-    loadData('lastScreen').then(
-      imageUri => {
-        console.log('LAST SCREEN', imageUri);
-        this.setState({
-          imageUri
-        })
-      }
-    )
+    loadData('lastScreen').then(imageUri => {
+      console.log('LAST SCREEN', imageUri);
+      this.setState({
+        imageUri,
+      });
+    });
   }
 
-  render () {
-    return <View style={{
-      backgroundColor: 'white',
-      borderRadius: 16,
-      height: '50%',
-      opacity: .3,
-      top: '25%',
-      left: 290,
-      position: 'absolute',
-      width: '50%'
-    }}>
-      <ImageBackground
-        source={{uri: this.props.lastSceneCapture}}
-        // source={{uri: "https://reactjs.org/logo-og.png"}}
+  render() {
+    return (
+      <View
         style={{
-          flex: 1,
-          justifyContent: "center",
-          resizeMode: "cover"
-        }}
-      />
+          backgroundColor: 'white',
+          borderRadius: 16,
+          height: '50%',
+          opacity: 0.3,
+          top: '25%',
+          left: 290,
+          position: 'absolute',
+          width: '50%',
+        }}>
+        <ImageBackground
+          source={{uri: this.props.lastSceneCapture}}
+          // source={{uri: "https://reactjs.org/logo-og.png"}}
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            resizeMode: 'cover',
+          }}
+        />
 
-      <Text style={{
-        color: 'red',
-        position: 'absolute',
-        left: -30
-      }}></Text>
-
-    </View>
+        <Text
+          style={{
+            color: 'red',
+            position: 'absolute',
+            left: -30,
+          }}></Text>
+      </View>
+    );
   }
 }
 
 class MainScene extends React.Component {
-
   platform = new Platform();
 
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  componentDidMount() {
-    console.log('FIP SCREEN HAS BEEN ENTERED');
-    this.checkForActiveUserSession();
-    this.loadData();
+  // componentDidMount() {
+  //   console.log('FIP SCREEN HAS BEEN ENTERED');
+  //   this.checkForActiveUserSession();
+  //   this.loadData();
 
-    setInterval(async () => {
-      const authTokenExpired = await this.checkSessionIsExpired();
-      if (authTokenExpired) {
-        this.props.navigation.replace('Logout')
-      }
-    }, 10000);
-  }
+  //   setInterval(async () => {
+  //     const authTokenExpired = await this.checkSessionIsExpired();
+  //     if (authTokenExpired) {
+  //       this.props.navigation.replace('Logout')
+  //     }
+  //   }, 10000);
+  // }
 
-  async checkSessionIsExpired() {
-    const { authTokenExpiry } = await retrieveAuthToken();
-    const currentDate = new Date();
-    return (currentDate.valueOf() / 1000) > JSON.parse(authTokenExpiry);
-  }
+  // async checkSessionIsExpired() {
+  //   const { authTokenExpiry } = await retrieveAuthToken();
+  //   const currentDate = new Date();
+  //   return (currentDate.valueOf() / 1000) > JSON.parse(authTokenExpiry);
+  // }
 
-  async checkForActiveUserSession() {
-    const { authToken } = await retrieveAuthToken();
-    if (!authToken) {
-      flashMessage(
-        APP_NAME,
-        EXPIRED_SESSION_MESSAGE,
-        BLOCKER
-      )
-      this.props.navigation.replace('Login');
-      return
-    }
-  }
+  // async checkForActiveUserSession() {
+  //   const { authToken } = await retrieveAuthToken();
+  //   if (!authToken) {
+  //     flashMessage(
+  //       APP_NAME,
+  //       EXPIRED_SESSION_MESSAGE,
+  //       BLOCKER
+  //     )
+  //     this.props.navigation.replace('Login');
+  //     return
+  //   }
+  // }
 
-  async loadAgent() {
-    const currentAgentResponse = await this.platform.getCurrentAgent();
-    const currentAgentResponseStatus = currentAgentResponse.status;
-    const currentAgentResponseObj = currentAgentResponse.response;
+  // async loadAgent() {
+  //   const currentAgentResponse = await this.platform.getCurrentAgent();
+  //   const currentAgentResponseStatus = currentAgentResponse.status;
+  //   const currentAgentResponseObj = currentAgentResponse.response;
 
-    console.log(currentAgentResponse);
+  //   console.log(currentAgentResponse);
 
-    if (currentAgentResponseStatus === ERROR_STATUS) {
-      flashMessage(
-        null,
-        await handleErrorResponse(currentAgentResponseObj),
-        BLOCKER
-      );
+  //   if (currentAgentResponseStatus === ERROR_STATUS) {
+  //     flashMessage(
+  //       null,
+  //       await handleErrorResponse(currentAgentResponseObj),
+  //       BLOCKER
+  //     );
 
-      this.props.navigation.replace('Logout')
+  //     this.props.navigation.replace('Logout')
 
-      return {}
+  //     return {}
 
-      return JSON.parse(await loadData(AGENT));
-    }
+  //     return JSON.parse(await loadData(AGENT));
+  //   }
 
-    await saveData(AGENT, currentAgentResponseObj);
-  }
+  //   await saveData(AGENT, currentAgentResponseObj);
+  // }
 
-  async loadApplication() {
-    const userData = JSON.parse(await loadData(USER));
+  // async loadApplication() {
+  //   const userData = JSON.parse(await loadData(USER));
 
-    const currentApplicationResponse = await this.onboarding.getApplicationByEmailOrPhone(userData.mobileNo);
-    const currentApplicationResponseStatus = currentApplicationResponse.status;
-    const currentApplicationResponseObj = currentApplicationResponse.response;
+  //   const currentApplicationResponse = await this.onboarding.getApplicationByEmailOrPhone(userData.mobileNo);
+  //   const currentApplicationResponseStatus = currentApplicationResponse.status;
+  //   const currentApplicationResponseObj = currentApplicationResponse.response;
 
-    console.log('GET APPLICATION RESPONSE', currentApplicationResponse);
+  //   console.log('GET APPLICATION RESPONSE', currentApplicationResponse);
 
-    if (currentApplicationResponseStatus === ERROR_STATUS) {
-      flashMessage(
-        null,
-        await handleErrorResponse(currentApplicationResponseObj),
-        BLOCKER
-      );
+  //   if (currentApplicationResponseStatus === ERROR_STATUS) {
+  //     flashMessage(
+  //       null,
+  //       await handleErrorResponse(currentApplicationResponseObj),
+  //       BLOCKER
+  //     );
 
-      this.props.navigation.replace('Logout')
+  //     this.props.navigation.replace('Logout')
 
-      return {}
+  //     return {}
 
-      return JSON.parse(await loadData(APPLICATION));
-    }
+  //     return JSON.parse(await loadData(APPLICATION));
+  //   }
 
-    await saveData(APPLICATION, currentApplicationResponseObj);
-  }
+  //   await saveData(APPLICATION, currentApplicationResponseObj);
+  // }
 
-  async loadUser() {
-    const currentUserResponse = await this.platform.getCurrentUser();
-    const currentUserResponseStatus = currentUserResponse.status;
-    const currentUserResponseObj = currentUserResponse.response;
+  // async loadUser() {
+  //   const currentUserResponse = await this.platform.getCurrentUser();
+  //   const currentUserResponseStatus = currentUserResponse.status;
+  //   const currentUserResponseObj = currentUserResponse.response;
 
-    console.log(currentUserResponse);
+  //   console.log(currentUserResponse);
 
-    if (currentUserResponseStatus === ERROR_STATUS) {
-      flashMessage(
-        null,
-        await handleErrorResponse(currentUserResponseObj),
-        BLOCKER
-      );
+  //   if (currentUserResponseStatus === ERROR_STATUS) {
+  //     flashMessage(
+  //       null,
+  //       await handleErrorResponse(currentUserResponseObj),
+  //       BLOCKER
+  //     );
 
-      this.props.navigation.replace('Logout')
+  //     this.props.navigation.replace('Logout')
 
-      return {}
-    }
+  //     return {}
+  //   }
 
-    await saveData(USER, currentUserResponseObj);
+  //   await saveData(USER, currentUserResponseObj);
 
-    return currentUserResponseObj;
-  }
+  //   return currentUserResponseObj;
+  // }
 
-  async loadData() {
-    this.setState({
-      isLoading: true
-    })
+  // async loadData() {
+  //   this.setState({
+  //     isLoading: true
+  //   })
 
-    const currentUser = await this.loadUser();
+  //   const currentUser = await this.loadUser();
 
-    const serializedUser = new UserSerializer(currentUser);
-    if (serializedUser.isAgent) {
-      await this.loadAgent();
-      // return
-    } else if (serializedUser.isApplicant) {
-      // await this.loadApplication();
-      // return
-    }
+  //   const serializedUser = new UserSerializer(currentUser);
+  //   if (serializedUser.isAgent) {
+  //     await this.loadAgent();
+  //     // return
+  //   } else if (serializedUser.isApplicant) {
+  //     // await this.loadApplication();
+  //     // return
+  //   }
 
-    this.setState({
-      isLoading: false
-    })
+  //   this.setState({
+  //     isLoading: false
+  //   })
 
-  }
-  
+  // }
+
   render() {
-    const { enable_fip } = this.props.remoteConfig;
+    const {enable_fip} = this.props.remoteConfig;
 
     const routes = {
-      'Home': <HomeScene />,
-      'Account': <AccountScene />,
-      'Application': <ApplicationScene
-        withNavigator 
-      />,
+      Home: <HomeScene />,
+      Account: <AccountScene />,
+      Application: <ApplicationScene withNavigator />,
       'Complete Setup': <CompleteSetupScene />,
-      'Pre-Setup Agent': <PreSetupAgentScene
-        withNavigator 
-      />,
+      'Pre-Setup Agent': <PreSetupAgentScene withNavigator />,
       'Monitoring And Support': <MonitoringAndSupportScene />,
       'Sync Data': <SettingsScene />,
-      'Settings': <SettingsScene />,
-      'Logout': <LogoutScene navigation={this.props.navigation} />
-    }
+      Settings: <SettingsScene />,
+      Logout: <LogoutScene navigation={this.props.navigation} />,
+    };
 
-    return enable_fip === false 
-      ? <DisabledScene sceneName="FIP features" includeLogoutButton includeStatusBar /> 
-      : <View style={{
-        height: '100%',
-        position: 'absolute',
-        width: '100%'
-      }}>
+    return enable_fip === false ? (
+      <DisabledScene
+        sceneName="FIP features"
+        includeLogoutButton
+        includeStatusBar
+      />
+    ) : (
+      <View
+        style={{
+          height: '100%',
+          position: 'absolute',
+          width: '100%',
+        }}>
         <UnderlayNavigator />
         <OverlaySwitchClone_ />
         <OverlaySwitch routes={routes} />
       </View>
+    );
   }
 }
 
@@ -248,7 +242,7 @@ function mapStateToProps(state) {
     isNavigatorVisible: state.navigation.isNavigatorVisible,
     lastSceneCapture: state.navigation.lastSceneCapture,
     remoteConfig: state.tunnel.remoteConfig,
-  }
+  };
 }
 
 const OverlaySwitchClone_ = connect(mapStateToProps, null)(OverlaySwitchClone);
