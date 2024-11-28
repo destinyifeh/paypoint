ea4b6bf013ef916c73dd8e3f6b2eb91dbfaf2db2
Destiny Ifehor <destiny.ifehor@interswitchgroup.com>
2024-11-28 16:10:56 +0000

    updated

--- src/fragments/update-banner.js
+++ src/fragments/update-banner.js
@@ -1,62 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect} from 'react';
 
-import { connect } from 'react-redux';
-import { Animated, Easing, Linking, View } from 'react-native';
+import {Animated, Easing, Linking} from 'react-native';
+import {connect} from 'react-redux';
 
 import Button from '../components/button';
 import Text from '../components/text';
-import { COLOUR_BLACK, COLOUR_GREEN } from '../constants/styles';
-import { APP_VERSION, ENVIRONMENT_IS_PRODUCTION } from '../constants/api-resources';
-import { shouldForceAppUpdate } from '../scenes/misc/important-update-available';
-
+import {
+  APP_VERSION,
+  ENVIRONMENT_IS_PRODUCTION,
+} from '../constants/api-resources';
+import {COLOUR_BLACK, COLOUR_GREEN} from '../constants/styles';
+import {shouldForceAppUpdate} from '../scenes/misc/important-update-available';
 
 function UpdateBanner({remoteConfig}) {
-
   const opacity = new Animated.Value(1);
   const translateY = new Animated.Value(0);
-  const isUpdateAvailable = remoteConfig.latest_app_version && remoteConfig.latest_app_version !== APP_VERSION;
-  const toShowBanner = (
-    isUpdateAvailable 
-    && !shouldForceAppUpdate({...remoteConfig})
-    && remoteConfig.show_app_update_banner 
-    && ENVIRONMENT_IS_PRODUCTION
-  );
- 
+  const isUpdateAvailable =
+    remoteConfig.latest_app_version &&
+    remoteConfig.latest_app_version !== APP_VERSION;
+  const toShowBanner =
+    isUpdateAvailable &&
+    !shouldForceAppUpdate({...remoteConfig}) &&
+    remoteConfig.show_app_update_banner &&
+    ENVIRONMENT_IS_PRODUCTION;
+
   useEffect(() => {
-    Animated.timing(
-      translateY,
-      {
-        toValue: -170,
-        duration: 200,
-        easing: Easing.exp,
-        useNativeDriver: true,
-      }
-    ).start();
+    console.log(APP_VERSION, 'app_vv');
+    console.log(remoteConfig.latest_app_version, 'remote_app_v');
+    Animated.timing(translateY, {
+      toValue: -170,
+      duration: 200,
+      easing: Easing.exp,
+      useNativeDriver: true,
+    }).start();
   });
 
   if (!toShowBanner) {
-    return <React.Fragment />
+    return <React.Fragment />;
   } else {
     setTimeout(() => {
-      Animated.timing(
-        opacity,
-        {
-          toValue: 0,
-          duration: 200,
-          easing: Easing.exp,
-          useNativeDriver: true,
-        }
-      ).start();
-      
-      Animated.timing(
-        translateY,
-        {
-          toValue: 0,
-          duration: 200,
-          easing: Easing.exp,
-          useNativeDriver: true,
-        }
-      ).start()
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 200,
+        easing: Easing.exp,
+        useNativeDriver: true,
+      }).start();
+
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 200,
+        easing: Easing.exp,
+        useNativeDriver: true,
+      }).start();
     }, 6500);
   }
 
@@ -65,26 +60,29 @@ function UpdateBanner({remoteConfig}) {
       style={{
         alignItems: 'center',
         alignSelf: 'center',
-        backgroundColor: COLOUR_BLACK, 
-        borderRadius: 4, 
+        backgroundColor: COLOUR_BLACK,
+        borderRadius: 4,
         // bottom: 60,
         bottom: -80,
         elevation: 4,
         flexDirection: 'row',
-        height: 72, 
+        height: 72,
         justifyContent: 'space-between',
-        maxWidth: 420, 
+        maxWidth: 420,
         opacity,
-        paddingHorizontal: 16, 
-        position: 'absolute', 
-        transform: [{
-          translateY
-        }],
-        width: '92%'
-      }}
-    >
-      <Text bold title white>An update is available</Text>
-      <Button  
+        paddingHorizontal: 16,
+        position: 'absolute',
+        transform: [
+          {
+            translateY,
+          },
+        ],
+        width: '92%',
+      }}>
+      <Text bold title white>
+        An update is available
+      </Text>
+      <Button
         buttonStyle={{
           paddingRight: 0,
         }}
@@ -94,7 +92,7 @@ function UpdateBanner({remoteConfig}) {
         onPress={() => Linking.openURL(remoteConfig.latest_app_url)}
         title="DOWNLOAD"
         titleStyle={{
-          color: COLOUR_GREEN
+          color: COLOUR_GREEN,
         }}
         transparent
       />
@@ -105,7 +103,7 @@ function UpdateBanner({remoteConfig}) {
 function mapStateToProps(state) {
   return {
     remoteConfig: state.tunnel.remoteConfig,
-  }
+  };
 }
 
 export default connect(mapStateToProps, null)(UpdateBanner);
