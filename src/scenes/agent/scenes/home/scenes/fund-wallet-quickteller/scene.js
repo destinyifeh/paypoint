92d56301b44029b2163c6e7f5c0803e8cad6855d
Destiny Ifehor <destiny.ifehor@interswitchgroup.com>
2024-12-03 11:55:47 +0000

    updated

--- src/scenes/agent/scenes/home/scenes/fund-wallet-quickteller/scene.js
+++ src/scenes/agent/scenes/home/scenes/fund-wallet-quickteller/scene.js
@@ -1,54 +1,59 @@
 import React from 'react';
-import {
-  StatusBar, 
-  View 
-} from 'react-native';
-import {
-  COLOUR_WHITE, CONTENT_DARK, CONTENT_LIGHT, COLOUR_RED, COLOUR_BLUE
-} from '../../../../../../constants/styles';
-import Header from '../../../../../../components/header';
+import {View} from 'react-native';
+import {Icon} from 'react-native-elements';
 import WebView from 'react-native-webview';
 import ActivityIndicator from '../../../../../../components/activity-indicator';
-import { Icon } from 'react-native-elements';
+import Header from '../../../../../../components/header';
+import {
+  COLOUR_BLUE,
+  COLOUR_RED,
+  COLOUR_WHITE,
+  CONTENT_LIGHT,
+} from '../../../../../../constants/styles';
 
 export default class FundWalletQuicktellerScene extends React.Component {
-  state = {
-
-  }
-
+  state = {};
   render() {
-    return <View style={{
-      flex: 1
-    }}>
-      <Header 
-        containerStyle={{
-          backgroundColor: COLOUR_BLUE
-        }}
-        navigationIconColor={COLOUR_WHITE}
-        leftComponent={<Icon
-          color={COLOUR_RED}
-          name="chevron-left"
-          onPress={() => this.props.navigation.goBack()}
-          size={40}
-          type="material"
-          underlayColor="transparent"
-        />}
-        statusBarProps={{
-          backgroundColor: 'transparent',
-          barStyle: CONTENT_LIGHT
-        }}
-        title="Fund Wallet"
-        titleStyle={{
-          color: COLOUR_WHITE,
-          fontWeight: 'bold'
-        }} />
-      <WebView
-        renderLoading={() => <View style={{height: '100%'}}>
-          <ActivityIndicator />
-        </View>}
-        source={{uri: this.props.fundWalletUrl}}
-        startInLoadingState={true}
-      />
-    </View> 
+    return (
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <Header
+          containerStyle={{
+            backgroundColor: COLOUR_BLUE,
+          }}
+          navigationIconColor={COLOUR_WHITE}
+          leftComponent={
+            <Icon
+              color={COLOUR_RED}
+              name="chevron-left"
+              onPress={() => this.props.navigation.goBack()}
+              size={40}
+              type="material"
+              underlayColor="transparent"
+            />
+          }
+          statusBarProps={{
+            backgroundColor: 'transparent',
+            barStyle: CONTENT_LIGHT,
+          }}
+          title="Fund Wallet"
+          titleStyle={{
+            color: COLOUR_WHITE,
+            fontWeight: 'bold',
+          }}
+        />
+        <WebView
+          renderLoading={() => (
+            <View style={{height: '100%'}}>
+              <ActivityIndicator />
+            </View>
+          )}
+          source={{uri: 'https://www.quickteller.com/quicktellerpaypoint'}}
+          startInLoadingState={true}
+        />
+      </View>
+    );
   }
 }
